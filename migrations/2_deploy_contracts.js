44776b2b596894c16770e087585a7903acc3c1b8
Eugence1117 <eugencelim@gmail.com>
2021-12-13 19:15:44 +0800

    Comment deployment code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,9 @@ var token = artifacts.require("Token");
 module.exports = function(deployer){
     deployer.deploy(dex);
 
-    deployer.deploy(token,"BTC","Bat Coin",6,99999 * (10 ** 6));
-    deployer.deploy(token,"SPC","Spider Coin",6,99999 * (10 ** 6));
-    deployer.deploy(token,"IRC","Erum Coin",4,99999 * (10 ** 4));
-    deployer.deploy(token,"SUH","Sushi",4,99999 * (10 ** 4));
-    deployer.deploy(token,"TK","Tok",4,99999 * (10 ** 4));
+    // deployer.deploy(token,"BTC","Bat Coin",6,99999 * (10 ** 6));
+    // deployer.deploy(token,"SPC","Spider Coin",6,99999 * (10 ** 6));
+    // deployer.deploy(token,"IRC","Erum Coin",4,99999 * (10 ** 4));
+    // deployer.deploy(token,"SUH","Sushi",4,99999 * (10 ** 4));
+    // deployer.deploy(token,"TK","Tok",4,99999 * (10 ** 4));
 };
